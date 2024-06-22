101d1bf9ce34a61b3977950738bde56f31331851
Vitaliy-datascientist <vitaliy.dayneko2410@gmail.com>
2024-06-22 05:04:37 +0300

    third task

---
+++ third_task/functionality.py
@@ -0,0 +1,38 @@
+"""Головний функціонал"""
+
+import re
+from pathlib import Path
+from colorama import Fore
+import sys
+
+
+def print_directories_and_files(path, sep, last_length):
+    """Функція виводить в терміналі всі піддиректорії та файли у вказаній директорії з використанням різних
+                          кольорів для піддиректорій та файлів, що полегшить візуальне сприйняття файлової структури."""
+    path = Path(path)
+
+    all_objects = list(path.iterdir())
+
+    for item in all_objects:
+        length = len(str(item).split('\\'))
+        dif = length - last_length
+
+        if item.is_dir():
+            print(Fore.MAGENTA + sep + str(item))
+            if dif >= 0:
+                print_directories_and_files(item, sep=sep + ('------|' * dif), last_length=length)
+            elif dif < 0:
+                print_directories_and_files(item, sep=sep.replace('------|', '', dif * dif), last_length=length)
+
+    for item in all_objects:
+        if item.is_file():
+            print(Fore.CYAN + sep + str(item))
+    print()
+
+
+def get_path_and_length() -> type[str, int]:
+    """Функція обробляє та повертає отриманий шлях та кількість рядків розділені '\' чи '/'"""
+    pattern = r'[\\/]+'
+    path = sys.argv[1]
+    length = len(re.split(pattern, path))
+    return path, length
---
+++ third_task/main.py
@@ -0,0 +1,21 @@
+"""Третє завдання.
+Скрипт приймає шлях до директорії в якості аргументу командного
+                          рядка і візуалізує структуру цієї директорії, виводячи імена всіх піддиректорій та файлів."""
+
+from functionality import print_directories_and_files, get_path_and_length
+
+
+def main():
+    """Головна функція"""
+    sep = '------|'
+    try:
+        path, length = get_path_and_length()
+        print_directories_and_files(path, sep, length)
+    except FileNotFoundError:
+        print(f'{path}\nДиректорію не знайдено')
+    except NotADirectoryError:
+        print(f'{path}\nНе є директорією')
+
+
+if __name__ == '__main__':
+    main()
