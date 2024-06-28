7cfd09b00be7d21b0f70b6f34027fa527270f2db
Vitaliy-datascientist <vitaliy.dayneko2410@gmail.com>
2024-06-28 15:32:59 +0300

    firth task(updated 5)

--- fourth_task/main.py
+++ fourth_task/main.py
@@ -3,18 +3,35 @@
                                     та відповідає відповідно до введеної команди."""
 
 
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Enter the argument for the command."
+        except KeyError:
+            return 'No contacts found under the name.'
+        except IndexError:
+            return "Enter the argument for the command."
+
+    return inner
+
+
+@input_error
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
 
+@input_error
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return 'Contact added.'
 
 
+@input_error
 def change_contact(args, contacts):
     name, phone = args
     if name in contacts:
@@ -24,17 +41,13 @@ def change_contact(args, contacts):
         return f'No contacts found under the name: {name}.'
 
 
+@input_error
 def show_phone(args, contacts):
-    if not len(args) == 1:
-        return 'Incorrect arguments, try again.'
-    else:
-        name = args[0]
-        if name in contacts:
-            return contacts[name]
-        else:
-            return f'No contacts found under the name {name}.'
+    name = args[0]
+    return contacts[name]
 
 
+@input_error
 def show_all_contacts(args, contacts):
     if args:
         return 'The command accepts no arguments.'
@@ -42,7 +55,7 @@ def show_all_contacts(args, contacts):
         return 'No contacts.'
     res = ''
     for key in contacts.keys():
-        res += f'{key} - {contacts[key]}\n'
+        res += f'{key}: {contacts[key]}\n'
     return res
 
 
@@ -52,24 +65,22 @@ def main():
     while True:
         user_input = input('Enter a command:')
         command, *args = parse_input(user_input)
-        try:
-            if command in ['close', 'exit']:
-                print('Good bye!')
-                break
-            elif command == 'hello':
-                print('How can I help you?')
-            elif command == 'add':
-                print(add_contact(args, contacts))
-            elif command == 'change':
-                print(change_contact(args, contacts))
-            elif command == 'phone':
-                print(show_phone(args, contacts))
-            elif command == 'all':
-                print(show_all_contacts(args, contacts))
-            else:
-                print('Invalid command.')
-        except ValueError:
-            print('Incorrect arguments, try again.')
+
+        if command in ['close', 'exit']:
+            print('Good bye!')
+            break
+        elif command == 'hello':
+            print('How can I help you?')
+        elif command == 'add':
+            print(add_contact(args, contacts))
+        elif command == 'change':
+            print(change_contact(args, contacts))
+        elif command == 'phone':
+            print(show_phone(args, contacts))
+        elif command == 'all':
+            print(show_all_contacts(args, contacts))
+        else:
+            print('Invalid command.')
 
 
 if __name__ == '__main__':
